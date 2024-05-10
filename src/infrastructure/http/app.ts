9fb736ebbefd91ace8ed502879038ed30ac67dce
KolesnykMA <maxymura@gmail.com>
2024-05-10 21:03:43 +0200

    Move router creation to expressapp class

--- src/infrastructure/http/app.ts
+++ src/infrastructure/http/app.ts
@@ -1,4 +1,12 @@
 import express, { Express } from 'express';
+import {
+  CreateUserController,
+  GetAllUsersController,
+  GetUserController,
+} from './users/controllers';
+import { GetAllUsersUseCase } from '../../application/use-cases/users/get-all/case';
+import { GetUserUseCase } from '../../application/use-cases/users/get/case';
+import { CreateUserUseCase } from '../../application/use-cases/users/create/case';
 
 export class ExpressApp {
   app: Express;
@@ -7,6 +15,22 @@ export class ExpressApp {
   constructor(config: { port: number }) {
     this.config = config;
     this.app = express();
+  }
+
+  registerUsers({
+    getAll,
+    get,
+    create,
+  }: {
+    getAll: GetAllUsersUseCase;
+    get: GetUserUseCase;
+    create: CreateUserUseCase;
+  }) {
+    const router = express.Router();
+    router.get('/users/', new GetAllUsersController(getAll).handle);
+    router.get('/users/:id', new GetUserController(get).handle);
+    router.post('/users', new CreateUserController(create).handle);
+    this.app.use(router);
   }
 
   start() {
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,9 @@
 import { ArrayStorageUsersRepo } from './infrastructure/dal/array-storage/users';
 import { ObjectStorageUsersRepo } from './infrastructure/dal/object-storage/users';
-import {
-  CreateUserController,
-  GetAllUsersController,
-  GetUserController,
-} from './infrastructure/http/users/controllers';
 import { GetUserUseCase } from './application/use-cases/users/get/case';
 import { ExpressApp } from './infrastructure/http/app';
 import { CreateUserUseCase } from './application/use-cases/users/create/case';
 import { Config } from './config';
-import express from 'express';
 import { GetAllUsersUseCase } from './application/use-cases/users/get-all/case';
 
 async function main() {
@@ -20,22 +14,11 @@ async function main() {
 
   const expressApp = new ExpressApp(config);
 
-  const usersRouter = express.Router();
-  usersRouter.get(
-    '/users/',
-    new GetAllUsersController(new GetAllUsersUseCase(arrayStorageUsersRepo))
-      .handle
-  );
-  usersRouter.get(
-    '/users/:id',
-    new GetUserController(new GetUserUseCase(arrayStorageUsersRepo)).handle
-  );
-  usersRouter.post(
-    '/users',
-    new CreateUserController(new CreateUserUseCase(objectStorageUsersRepo))
-      .handle
-  );
-  expressApp.app.use(usersRouter);
+  expressApp.registerUsers({
+    getAll: new GetAllUsersUseCase(arrayStorageUsersRepo),
+    get: new GetUserUseCase(arrayStorageUsersRepo),
+    create: new CreateUserUseCase(objectStorageUsersRepo),
+  });
 
   expressApp.start();
 }
